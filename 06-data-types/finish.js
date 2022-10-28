aeda7e91573877778c5c5ced1374061eaf000af9
Anatolii Horobets <admin@cheapsite.studio>
2022-10-28 21:14:32 +0300

    added task 06

--- 06-data-types/finish.js
+++ 06-data-types/finish.js
@@ -12,3 +12,24 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+const str = 'this is a string!';
+console.log(typeof str);
+
+const num = 10;
+console.log(typeof num);
+
+const bool = true;
+console.log(typeof bool);
+
+const nulled = null;
+console.log(typeof nulled);
+
+let undef;
+console.log(typeof undef);
+
+const arr = [];
+console.log(typeof arr);
+
+const obj = {};
+console.log(typeof obj);
